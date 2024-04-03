3c36f9fb7bdd212746e68fcd2769e356ced7233b
levanthang23092002 <levanthang230902@gmail.com>
2024-04-03 21:41:47 +0700

    Homework 43

--- GD3/homeworks/controllers/userController.js
+++ GD3/homeworks/controllers/userController.js
@@ -4,18 +4,27 @@ exports.getAllHotel = (req, res) => {
   User.getAllHotel((error, results) => {
     if (error) {
       res.status(500).json({
-        success: "Thất Bại",
+        success: false,
         message: 'Internal server error',
         data: {},
         error_code: error
       });
     } else {
-      res.json({
-        success: "Thành Công",
-        message: 'Success',
-        data: results,
-        error_code: ''
-      });
+      if (results.length === 0) {
+        res.status(404).json({
+          success: false,
+          message: 'No hotels found',
+          data: {},
+          error_code: ''
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'Successfully retrieved hotels',
+          data: results,
+          error_code: ''
+        });
+      }
     }
   });
 };
@@ -24,18 +33,27 @@ exports.getAllRoom = (req, res) => {
   User.getAllRoom((error, results) => {
     if (error) {
       res.status(500).json({
-        success: "Thất Bại",
+        success: false,
         message: 'Internal server error',
         data: {},
         error_code: error
       });
     } else {
-      res.json({
-        success: "Thành Công",
-        message: 'Success',
-        data: results,
-        error_code: ''
-      });
+      if (results.length === 0) {
+        res.status(404).json({
+          success: false,
+          message: 'No rooms found',
+          data: {},
+          error_code: ''
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'Successfully retrieved rooms',
+          data: results,
+          error_code: ''
+        });
+      }
     }
   });
 };
@@ -46,60 +64,87 @@ exports.getUser = (req, res) => {
   User.getUser(userId, (error, results) => {
     if (error) {
       res.status(500).json({
-        success: "Thất Bại",
+        success: false,
         message: 'Internal server error',
         data: {},
         error_code: error
       });
     } else {
-      res.json({
-        success: "Thành Công",
-        message: 'Success',
-        data: results,
-        error_code: ''
-      });
+      if (results.length === 0) {
+        res.status(404).json({
+          success: false,
+          message: 'User not found',
+          data: {},
+          error_code: ''
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'Successfully retrieved user',
+          data: results,
+          error_code: ''
+        });
+      }
     }
   });
 };
 
 exports.getAllRoomBooking = (req, res) => {
   const userId = req.params.id;
-  User.getAllRoomBooking(userId,(error, results) => {
+  User.getAllRoomBooking(userId, (error, results) => {
     if (error) {
       res.status(500).json({
-        success: "Thất Bại",
+        success: false,
         message: 'Internal server error',
         data: {},
         error_code: error
       });
     } else {
-      res.json({
-        success: "Thành Công",
-        message: 'Success',
-        data: results,
-        error_code: ''
-      });
+      if (results.length === 0) {
+        res.status(404).json({
+          success: false,
+          message: 'No room bookings found',
+          data: {},
+          error_code: ''
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'Successfully retrieved room bookings',
+          data: results,
+          error_code: ''
+        });
+      }
     }
   });
 };
 
 exports.getRoomBooking = (req, res) => {
   const roomId = req.params.id;
-  User.getRoomBooking( roomId, (error, results) => {
+  User.getRoomBooking(roomId, (error, results) => {
     if (error) {
       res.status(500).json({
-        success: "Thất Bại",
+        success: false,
         message: 'Internal server error',
         data: {},
         error_code: error
       });
     } else {
-      res.json({
-        success: "Thành Công",
-        message: 'Success',
-        data: results,
-        error_code: ''
-      });
+      if (results.length === 0) {
+        res.status(404).json({
+          success: false,
+          message: 'Room booking not found',
+          data: {},
+          error_code: ''
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'Successfully retrieved room booking',
+          data: results,
+          error_code: ''
+        });
+      }
     }
   });
 };
@@ -108,18 +153,27 @@ exports.getAllEvaluate = (req, res) => {
   User.getAllEvaluate((error, results) => {
     if (error) {
       res.status(500).json({
-        success: "Thất Bại",
+        success: false,
         message: 'Internal server error',
         data: {},
         error_code: error
       });
     } else {
-      res.json({
-        success: "Thành Công",
-        message: 'Success',
-        data: results,
-        error_code: ''
-      });
+      if (results.length === 0) {
+        res.status(404).json({
+          success: false,
+          message: 'No evaluations found',
+          data: {},
+          error_code: ''
+        });
+      } else {
+        res.status(200).json({
+          success: true,
+          message: 'Successfully retrieved evaluations',
+          data: results,
+          error_code: ''
+        })
+      }
     }
-  });
-};
+  })
+}
