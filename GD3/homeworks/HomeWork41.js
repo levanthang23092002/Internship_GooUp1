16f226e02cc8481a4ba04aa7b5302c4ba52aae9d
levanthang23092002 <levanthang230902@gmail.com>
2024-04-05 21:26:18 +0700

    Homework 44

--- GD3/homeworks/HomeWork41.js
+++ GD3/homeworks/HomeWork41.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
+const cors = require('cors');
+const helmet = require('helmet');
 
+
+
+const corsOptions = {
+  origin: '*', // Chỉ cho phép truy cập từ tất cả các nguồn 
+  methods: ['GET', 'POST'], // Chỉ cho phép các phương thức GET và POST
+  allowedHeaders: ['Content-Type', 'Authorization'], // Chỉ cho phép các header Content-Type và Authorization
+}
+
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use('/api', apiRoutes);
-
-
 const isDev = process.env.NODE_ENV === 'development';
 
 if (isDev) {
