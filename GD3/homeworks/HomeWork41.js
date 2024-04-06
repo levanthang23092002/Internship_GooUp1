dd820943ff98185bb459c8d0c7f7dc533b5323dc
levanthang23092002 <levanthang230902@gmail.com>
2024-04-06 19:32:44 +0700

    update homework 42

--- GD3/homeworks/HomeWork41.js
+++ GD3/homeworks/HomeWork41.js
@@ -15,20 +15,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(helmet());
 app.use('/api', apiRoutes);
-const isDev = process.env.NODE_ENV === 'development';
 
-if (isDev) {
-  const nodemon = require('nodemon');
-  nodemon({ script: 'server.js' });
-  nodemon.on('start', () => {
-    console.log('Server is running with auto-reload');
-  });
-  nodemon.on('crash', () => {
-    console.log('Server crashed, restarting...');
-  });
-} else {
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => {
-    console.log(`Server đang lắng nghe tại http://localhost:${port}`);
-  });
-}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server đang lắng nghe tại http://localhost:${port}`);
+});
